8ea6ff01c6a72cb35b0b93272e9250a99803f655
pabperort1 <pabperort1@alum.us.es>
2022-11-26 13:37:21 +0100

    Base de datos de prueba hecha

--- database.py
+++ database.py
@@ -4,13 +4,23 @@ con = sqlite3.connect('data.db')
 cur = con.cursor()
 
 # Create table
-cur.execute('''CREATE TABLE usuario
-               (mensaje text, id_empleado integer, firma text)''')
+# cur.execute('''CREATE TABLE usuario
+#                (mensaje text, id_empleado integer, firma text)''')
 
 # Insert a row of data
-cur.execute("INSERT INTO usuario VALUES ('mensaje_de_prueba','1','firmafirma')")
+#cur.execute("INSERT INTO usuario VALUES ('mensaje_de_prueba2','2','firmafirma2')")
 
+############################Por si queremos añadir mucha gente a la vez
+# data = [
+#     ("Monty Python Live at the Hollywood Bowl", 1982, 7.9),
+#     ("Monty Python's The Meaning of Life", 1983, 7.5),
+#     ("Monty Python's Life of Brian", 1979, 8.0),
+# ]
+# cur.executemany("INSERT INTO movie VALUES(?, ?, ?)", data)
+# con.commit()  # Remember to commit the transaction after executing INSERT.
 
+res = cur.execute("SELECT * FROM usuario")
+print(res.fetchall())
 # Save (commit) the changes
 con.commit()
 
