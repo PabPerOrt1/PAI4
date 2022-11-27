390637b4514b60e91b56e54d68737d41904d97e7
pabperort1 <pabperort1@alum.us.es>
2022-11-27 13:28:39 +0100

    Conversion server a java

---
+++ DB.java
@@ -0,0 +1,24 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DB {
+ 
+   public static void main ( String args[] ){
+
+      Connection connection = null;
+      
+
+      try{
+         Class.forName("org.sqlite.date.org.sqlite.date.DateFormatUtils");
+         connection = DriverManager.getConnection( "jdbc:sqlite:mydatabase.db" );
+         if ( connection != null ){
+            System.out.println("Conexión exitosa!");
+         }
+      }
+      catch ( Exception ex ) {
+         System.err.println( ex.getClass().getName() + ": " + ex.getMessage() );
+         System.out.println("Error en la conexión");
+      }
+   }
+  
+ }
---
+++ mysql.sql
@@ -0,0 +1,5 @@
+-- CREATE TABLE valores(id_empleado INTEGER NOT NULL PRIMARY KEY, 
+-- camas INTEGER NOT NULL, sillas INTEGER NOT NULL, 
+-- sillones INTEGER NOT NULL, mesas INTEGER NOT NULL, date DATETIME NOT NULL, accepted TEXT );
+
+INSERT INTO valores(id_empleado,)
